3e2da886c5ae568da1f925790cb1bc993cbca0b5
Jonathan <jpreuven@gmail.com>
2023-07-05 13:15:10 -0700

    COMPLETED LABS

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [itemClass, setItemClass] = useState("");
+  const [buttonClass, setButtonClass] = useState("add");
+
+  function handleItemClass() {
+    if (itemClass === "") {
+      setItemClass("in-cart");
+      name = "Remove from Cart";
+    } else {
+      setItemClass("");
+      name = "Add to Cart";
+    }
+  }
+
+  function handleButtonClass() {
+    if (buttonClass === "add") {
+      setButtonClass("remove");
+    } else {
+      setButtonClass("add");
+    }
+  }
+
+  const buttonText = buttonClass === "add" ? "Add to Cart" : "Remove From Cart";
+
   return (
-    <li className="">
+    <li className={itemClass} onClick={handleItemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={handleButtonClass}>
+        {buttonText}
+      </button>
     </li>
   );
 }
